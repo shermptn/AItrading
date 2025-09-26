0d0723fdc3072cf36623871b29a0806d3936994b
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-25 18:19:35 -0700

    Update MarketPulsePage.tsx

--- src/pages/MarketPulsePage.tsx
+++ src/pages/MarketPulsePage.tsx
@@ -12,22 +12,22 @@ export default function MarketPulsePage() {
         </p>
       </header>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Column 1: Index Futures */}
         <div>
           <TradingViewWidget
             widgetSrc="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
             widgetConfig={{
               width: "100%",
-              height: 400,
+              height: 500,
               colorTheme: "dark",
               symbolsGroups: [
                 {
                   name: "Index Futures",
                   symbols: [
-                    { name: "CME_MINI:ES1!", displayName: "S&P 500 Futures" },
-                    { name: "CME_MINI:NQ1!", displayName: "Nasdaq 100 Futures" },
-                    { name: "CBOT:YM1!", displayName: "Dow 30 Futures" },
+                    { name: "SPX", displayName: "S&P 500 Index" },
+                    { name: "NDX", displayName: "Nasdaq 100 Index" },
+                    { name: "DJI", displayName: "Dow 30 Index" }
                   ],
                 },
               ],
@@ -35,7 +35,7 @@ export default function MarketPulsePage() {
               isTransparent: false,
               locale: "en",
             }}
-            height={400}
+            height={500}
           />
         </div>
 
@@ -45,14 +45,14 @@ export default function MarketPulsePage() {
             widgetSrc="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
             widgetConfig={{
               width: "100%",
-              height: 400,
+              height: 500,
               colorTheme: "dark",
               symbolsGroups: [
                 {
                   name: "Treasury Yields",
                   symbols: [
                     { name: "TVC:US10Y", displayName: "US 10-Year Yield" },
-                    { name: "TVC:US02Y", displayName: "US 2-Year Yield" },
+                    { name: "TVC:US02Y", displayName: "US 2-Year Yield" }
                   ],
                 },
               ],
@@ -60,7 +60,7 @@ export default function MarketPulsePage() {
               isTransparent: false,
               locale: "en",
             }}
-            height={400}
+            height={500}
           />
         </div>
 
@@ -70,16 +70,15 @@ export default function MarketPulsePage() {
             widgetSrc="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
             widgetConfig={{
               width: "100%",
-              height: 400,
+              height: 500,
               colorTheme: "dark",
               symbolsGroups: [
                 {
                   name: "Commodities & Forex",
                   symbols: [
-                    { name: "NYMEX:CL1!", displayName: "Crude Oil (WTI)" },
-                    { name: "COMEX:GC1!", displayName: "Gold" },
                     { name: "TVC:DXY", displayName: "US Dollar Index" },
                     { name: "FX:EURUSD", displayName: "EUR/USD" },
+                    { name: "TVC:VIX", displayName: "Volatility Index (VIX)" },
                   ],
                 },
               ],
@@ -87,14 +86,45 @@ export default function MarketPulsePage() {
               isTransparent: false,
               locale: "en",
             }}
-            height={400}
+            height={500}
           />
         </div>
 
-        {/* Column 4: S&P 500 Sector Heatmap */}
+        {/* Column 4: Gold & Oil */}
         <div>
+          <TradingViewWidget
+            widgetSrc="https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js"
+            widgetConfig={{
+              width: "100%",
+              height: 500,
+              colorTheme: "dark",
+              symbolsGroups: [
+                {
+                  name: "Gold & Oil",
+                  symbols: [
+                    { name: "COMEX:GC1!", displayName: "Gold" },
+                    { name: "NYMEX:CL1!", displayName: "Crude Oil (WTI)" }
+                  ],
+                },
+              ],
+              showSymbolLogo: true,
+              isTransparent: false,
+              locale: "en",
+            }}
+            height={500}
+          />
+        </div>
+
+        {/* S&P 500 Sector Heatmap spans both columns and is taller */}
+        <div className="md:col-span-2">
           <HeatmapWidget />
         </div>
+      </div>
+
+      <div className="mt-4 text-center text-sm text-neutral-400">
+        <span>
+          <strong>Note:</strong> Some market data may be delayed or unavailable due to exchange licensing restrictions in TradingView widgets.
+        </span>
       </div>
     </div>
   );
