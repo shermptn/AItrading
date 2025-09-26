93abf5a397c6e336e06166f393707de509115adc
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-25 18:11:42 -0700

    Delete src/components/MarketPulse/FuturesWidget.tsx

--- src/components/MarketPulse/FuturesWidget.tsx
+++
@@ -1,6 +0,0 @@
-import MacroDataWidget from "./MacroDataWidget";
-import { FUTURES_SYMBOLS } from "../../config/marketPulseConfig";
-
-export default function FuturesWidget() {
-  return <MacroDataWidget title="Index Futures" symbols={FUTURES_SYMBOLS} />;
-}
