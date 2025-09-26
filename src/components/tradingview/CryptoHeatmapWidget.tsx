dd65d71bd94e32697f96703d8ea5e98ef265657b
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-26 13:44:55 -0700

    Update CryptoHeatmapWidget.tsx

--- src/components/tradingview/CryptoHeatmapWidget.tsx
+++ src/components/tradingview/CryptoHeatmapWidget.tsx
@@ -1,41 +1,21 @@
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useRef } from 'react';
+import { useTradingViewWidget } from '../../hooks/useTradingViewWidget';
+
+const widgetConfig = {
+  "dataSource": "Crypto", "blockSize": "market_cap_calc", "blockColor": "24h_close_change|5", "locale": "en",
+  "symbolUrl": "", "colorTheme": "dark", "hasTopBar": false, "isDataSetEnabled": false, "isZoomEnabled": true,
+  "hasSymbolTooltip": true, "isMonoSize": false, "width": "100%", "height": "100%"
+};
 
 function CryptoHeatmapWidget() {
-  const container = useRef();
+  const containerRef = useRef<HTMLDivElement>(null);
+  useTradingViewWidget({
+    widgetScriptSrc: "https://s3.tradingview.com/external-embedding/embed-widget-crypto-coins-heatmap.js",
+    widgetConfig,
+    containerRef
+  });
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-crypto-coins-heatmap.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
-        {
-          "dataSource": "Crypto",
-          "blockSize": "market_cap_calc",
-          "blockColor": "24h_close_change|5",
-          "locale": "en",
-          "symbolUrl": "",
-          "colorTheme": "dark",
-          "hasTopBar": false,
-          "isDataSetEnabled": false,
-          "isZoomEnabled": true,
-          "hasSymbolTooltip": true,
-          "isMonoSize": false,
-          "width": "100%",
-          "height": "100%"
-        }`;
-      container.current.appendChild(script);
-    },
-    []
-  );
-
-  return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/heatmap/crypto/" rel="noopener nofollow" target="_blank"><span className="blue-text">Crypto Heatmap</span></a><span className="trademark"> by TradingView</span></div>
-    </div>
-  );
+  return <div className="tradingview-widget-container" ref={containerRef} style={{ height: "100%", width: "100%" }}></div>;
 }
 
-export default memo(CryptoHeatmapWidget);
+export default React.memo(CryptoHeatmapWidget);
