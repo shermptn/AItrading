d651cd0efb67269f6f689cb658aa9cc855881637
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-26 13:46:57 -0700

    Update MarketQuotesWidget.tsx

--- src/components/tradingview/MarketQuotesWidget.tsx
+++ src/components/tradingview/MarketQuotesWidget.tsx
@@ -1,139 +1,31 @@
-import React, { useEffect, useRef, memo } from 'react';
+import React, { useRef } from 'react';
+import { useTradingViewWidget } from '../../hooks/useTradingViewWidget';
 
-function MarketQuotesWidget() {
-  const container = useRef();
+const widgetConfig = {
+  "symbols": [
+    {"proName": "FOREXCOM:SPXUSD", "title": "S&P 500"},
+    {"proName": "FOREXCOM:NSXUSD", "title": "US 100"},
+    {"proName": "BITSTAMP:BTCUSD", "title": "Bitcoin"},
+    {"proName": "BITSTAMP:ETHUSD", "title": "Ethereum"},
+    {"proName": "NASDAQ:TSLA", "title": "Tesla"},
+    {"proName": "NASDAQ:META", "title": "Meta"},
+    {"proName": "NASDAQ:MSFT", "title": "Microsoft"},
+    {"proName": "OANDA:XAUUSD", "title": "Gold"},
+    {"proName": "NASDAQ:NVDA", "title": "NVIDIA"},
+    {"proName": "NASDAQ:AAPL", "title": "Apple"}
+  ],
+  "colorTheme": "dark", "locale": "en", "isTransparent": false, "showSymbolLogo": true, "displayMode": "adaptive"
+};
 
-  useEffect(
-    () => {
-      const script = document.createElement("script");
-      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-market-quotes.js";
-      script.type = "text/javascript";
-      script.async = true;
-      script.innerHTML = `
-        {
-          "colorTheme": "dark",
-          "locale": "en",
-          "largeChartUrl": "",
-          "isTransparent": false,
-          "showSymbolLogo": true,
-          "backgroundColor": "#0F0F0F",
-          "support_host": "https://www.tradingview.com",
-          "width": 550,
-          "height": 550,
-          "symbolsGroups": [
-            {
-              "name": "Indices",
-              "symbols": [
-                {
-                  "name": "FOREXCOM:SPXUSD",
-                  "displayName": "S&P 500 Index"
-                },
-                {
-                  "name": "FOREXCOM:NSXUSD",
-                  "displayName": "US 100 Cash CFD"
-                },
-                {
-                  "name": "FOREXCOM:DJI",
-                  "displayName": "Dow Jones Industrial Average Index"
-                },
-                {
-                  "name": "INDEX:NKY",
-                  "displayName": "Japan 225"
-                },
-                {
-                  "name": "INDEX:DEU40",
-                  "displayName": "DAX Index"
-                },
-                {
-                  "name": "FOREXCOM:UKXGBP",
-                  "displayName": "FTSE 100 Index"
-                }
-              ]
-            },
-            {
-              "name": "Futures",
-              "symbols": [
-                {
-                  "name": "BMFBOVESPA:ISP1!",
-                  "displayName": "S&P 500"
-                },
-                {
-                  "name": "BMFBOVESPA:EUR1!",
-                  "displayName": "Euro"
-                },
-                {
-                  "name": "CMCMARKETS:GOLD",
-                  "displayName": "Gold"
-                },
-                {
-                  "name": "PYTH:WTI3!",
-                  "displayName": "WTI Crude Oil"
-                },
-                {
-                  "name": "BMFBOVESPA:CCM1!",
-                  "displayName": "Corn"
-                }
-              ]
-            },
-            {
-              "name": "Bonds",
-              "symbols": [
-                {
-                  "name": "EUREX:FGBL1!",
-                  "displayName": "Euro Bund"
-                },
-                {
-                  "name": "EUREX:FBTP1!",
-                  "displayName": "Euro BTP"
-                },
-                {
-                  "name": "EUREX:FGBM1!",
-                  "displayName": "Euro BOBL"
-                }
-              ]
-            },
-            {
-              "name": "Forex",
-              "symbols": [
-                {
-                  "name": "FX:EURUSD",
-                  "displayName": "EUR to USD"
-                },
-                {
-                  "name": "FX:GBPUSD",
-                  "displayName": "GBP to USD"
-                },
-                {
-                  "name": "FX:USDJPY",
-                  "displayName": "USD to JPY"
-                },
-                {
-                  "name": "FX:USDCHF",
-                  "displayName": "USD to CHF"
-                },
-                {
-                  "name": "FX:AUDUSD",
-                  "displayName": "AUD to USD"
-                },
-                {
-                  "name": "FX:USDCAD",
-                  "displayName": "USD to CAD"
-                }
-              ]
-            }
-          ]
-        }`;
-      container.current.appendChild(script);
-    },
-    []
-  );
+function TickerTapeWidget() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  useTradingViewWidget({
+    widgetScriptSrc: "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js",
+    widgetConfig,
+    containerRef
+  });
 
-  return (
-    <div className="tradingview-widget-container" ref={container}>
-      <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/" rel="noopener nofollow" target="_blank"><span className="blue-text">Market summary</span></a><span className="trademark"> by TradingView</span></div>
-    </div>
-  );
+  return <div className="tradingview-widget-container" ref={containerRef} style={{ height: "72px", width: "100%" }}></div>;
 }
 
-export default memo(MarketQuotesWidget);
+export default React.memo(TickerTapeWidget);
