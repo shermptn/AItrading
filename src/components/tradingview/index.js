2a257c2a6f7e5cffbc7c6a581623a77df7e93463
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-26 13:14:06 -0700

    Add index.js to export trading view widgets

---
+++ src/components/tradingview/index.js
@@ -0,0 +1,8 @@
+export { default as AdvancedChartWidget } from './AdvancedChartWidget';
+export { default as StockHeatmapWidget } from './StockHeatmapWidget';
+export { default as CryptoHeatmapWidget } from './CryptoHeatmapWidget';
+export { default as EconomicCalendarWidget } from './EconomicCalendarWidget';
+export { default as TimelineWidget } from './TimelineWidget';
+export { default as ForexScreenerWidget } from './ForexScreenerWidget';
+export { default as TickerTapeWidget } from './TickerTapeWidget';
+export { default as MarketQuotesWidget } from './MarketQuotesWidget';
