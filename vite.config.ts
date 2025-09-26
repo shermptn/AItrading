8f79b0ddac5890a4c50b103ee01a9460bcefb2a2
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-25 18:51:35 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   build: {
-    target: 'esnext' // <-- Add this line to support top-level await!
-  }
-})
+    rollupOptions: {
+      external: ['react', 'react-dom'], // Add external dependencies here
+    },
+  },
+});
