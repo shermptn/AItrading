b511beb227ecaf317e7424bdc33711c0f6691ed3
shermptn <56558519+shermptn@users.noreply.github.com>
2025-09-26 13:11:17 -0700

    Add CryptoHeatmapWidget component

---
+++ src/components/tradingview/CryptoHeatmapWidget.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useRef, memo } from 'react';
+
+function CryptoHeatmapWidget() {
+  const container = useRef();
+
+  useEffect(
+    () => {
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-crypto-coins-heatmap.js";
+      script.type = "text/javascript";
+      script.async = true;
+      script.innerHTML = `
+        {
+          "dataSource": "Crypto",
+          "blockSize": "market_cap_calc",
+          "blockColor": "24h_close_change|5",
+          "locale": "en",
+          "symbolUrl": "",
+          "colorTheme": "dark",
+          "hasTopBar": false,
+          "isDataSetEnabled": false,
+          "isZoomEnabled": true,
+          "hasSymbolTooltip": true,
+          "isMonoSize": false,
+          "width": "100%",
+          "height": "100%"
+        }`;
+      container.current.appendChild(script);
+    },
+    []
+  );
+
+  return (
+    <div className="tradingview-widget-container" ref={container}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright"><a href="https://www.tradingview.com/heatmap/crypto/" rel="noopener nofollow" target="_blank"><span className="blue-text">Crypto Heatmap</span></a><span className="trademark"> by TradingView</span></div>
+    </div>
+  );
+}
+
+export default memo(CryptoHeatmapWidget);
